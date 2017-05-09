652d98f24c4b70e4b55322cc8fa052a4c1386cba
Umang Galaiya <umanghome@gmail.com>
2017-05-09 12:54:24 +0530

    Implementation for US addresses

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-### Express your concerns to FCC regarding Net Neutrality by just filling 3 fields! [International Addresses Only]
+### Express your concerns to FCC regarding Net Neutrality by just filling 3 fields!
 
 This is a Python application that shows a dead-simple HTML form that asks for a person's name, email, and address and submits a Comment to FCC regarding Net Neutrality.
-
-This works only for International Addresses for now. Feel free to raise a PR to support local addresses.
 
 Requires PhantomJS on the server to work.
 
@@ -12,4 +10,4 @@ On the server, do the following:
 3. `pip install splinter flask`
 4. `python server.py`
 
-`static/index.html` contains the HTML page that contains the form.
+`index.html` contains the HTML page that contains the form.
--- server.py
+++ server.py
@@ -1,20 +1,86 @@
 from flask import Flask, request, send_from_directory
 from splinter import Browser
+import sys
 
-FLASK_PORT = 80
+FLASK_PORT = 5005
+BROWSER_TO_USE = 'phantomjs'
+IP_TO_USE = '0.0.0.0'
+SERVER_NAME = 'ffd51ee2.ngrok.io'
+
+if len(sys.argv) != 4:
+    print 'Usage: python server.py <IP to use> <Port to use> <Server Name>'
+
+IP_TO_USE = sys.argv[1]
+FLASK_PORT = sys.argv[2]
+SERVER_NAME = sys.agrv[3]
 
 url = 'https://www.fcc.gov/ecfs/search/proceedings?q=name:((17-108))'
 
 app = Flask(__name__)
+app.config['SERVER_NAME'] = SERVER_NAME
 
-app.run(port=FLASK_PORT)
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index_route ():
-    if request.method == 'GET':
-        return send_from_directory('static', 'index.html')
+    return send_from_directory("", 'index.html')
+
+@app.route('/submit-us', methods=['POST'])
+def submit_us ():
     print request.form
-    browser = Browser('phantomjs')
+
+    required_keys = ['name', 'email', 'address', 'city', 'state', 'zip', 'comments']
+    for r in required_keys:
+        if not request.form.has_key(r):
+            return 'Please enter all fiels!', 400
+
+    browser = Browser(BROWSER_TO_USE)
+
+    # Visit URL
+    browser.visit(url)
+
+    # Click +Express
+    browser.find_by_css('#printElement > tbody > tr > td > div:nth-child(1) > div.col-lg-3.text-right > a:nth-child(2)').first.click()
+
+    # Fill Name
+    browser.find_by_css('#signupform > div:nth-child(3) > div.col-sm-10 > div > div > input').first.fill(request.form['name'] + '\n')
+
+    # Fill Email
+    browser.find_by_css('#email').first.fill(request.form['email'])
+
+    # Fill Address
+    browser.find_by_css('#address1').first.fill(request.form['address'])
+
+    # Fill City
+    browser.find_by_css('#city').first.fill(request.form['city'])
+
+    # Select State
+    browser.find_by_css('#userstate').first.select(request.form['state'])
+
+    # Fill Zip
+    browser.find_by_css('#zip').first.fill(request.form['zip'])
+
+    # Fill Comment
+    browser.find_by_css('#briefComment').fill(request.form['comments'])
+
+    # Submit
+    browser.find_by_css('#signupform > div.col-sm-offset-2 > div > div > a').first.click()
+    browser.find_by_css('#signupform > div:nth-child(8) > div > div > button').first.click()
+
+    # Get confirmation number
+    number = browser.find_by_css('#confirmation').first.text
+
+    # Show confirmation number
+    return 'Confirmation number: ' + number
+
+@app.route('/submit-international', methods=['POST'])
+def submit_international ():
+    print request.form
+
+    required_keys = ['name', 'email', 'address', 'comments']
+    for r in required_keys:
+        if not request.form.has_key(r):
+            return 'Please enter all fiels!', 400
+
+    browser = Browser(BROWSER_TO_USE)
 
     # Visit URL
     browser.visit(url)
@@ -44,5 +110,12 @@ def index_route ():
     # Get confirmation number
     number = browser.find_by_css('#confirmation').first.text
 
+    print number
+
+    browser.quit()
+
     # Show confirmation number
     return 'Confirmation number: ' + number
+
+if __name__ == '__main__':
+    app.run(port=FLASK_PORT)
